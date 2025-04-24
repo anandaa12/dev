e2eac2ccb5bf201fcc4057030cef16663e69f156
anandaa12 <anand.e145@gmail.com>
2025-04-24 04:39:38 +0000

    update

--- env/.config/nvim/lua/anadai/keymaps.lua
+++ env/.config/nvim/lua/anadai/keymaps.lua
@@ -10,3 +10,12 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
+
+-- Normal mode: Move line up/down
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down", noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up", noremap = true, silent = true })
+
+-- Visual mode: Move selected lines up/down
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", noremap = true, silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", noremap = true, silent = true })
+
