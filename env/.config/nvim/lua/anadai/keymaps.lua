7ed7a1fe251506296e41baeae1a9c5687859b81c
anandaa12 <anand.e145@gmail.com>
2025-06-02 15:10:26 +0800

    automated dev commit

--- env/.config/nvim/lua/anadai/keymaps.lua
+++ env/.config/nvim/lua/anadai/keymaps.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.g.maplocalloader = " " 
+vim.g.maplocalloader = " "
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -7,9 +7,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- For conciseness
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
 
 -- Normal mode: Move line up/down
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down", noremap = true, silent = true })
@@ -19,3 +19,8 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up", noremap = 
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", noremap = true, silent = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", noremap = true, silent = true })
 
+
+
+vim.keymap.set('n', '<C-z>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-z>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('v', '<C-z>', '<Nop>', { noremap = true, silent = true })
