a8b1c2efbf9f392a2d0d773c5cdf1c6c5351858f
anandaa12 <anand.e145@gmail.com>
2025-09-01 10:00:36 +0800

    automated dev commit

--- env/.config/nvim/lua/anadai/lazy/conform.lua
+++ env/.config/nvim/lua/anadai/lazy/conform.lua
@@ -35,7 +35,7 @@ return {
       },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
+    -- format_on_save = { timeout_ms = 500 },
     -- Set default options
     default_format_opts = {
       lsp_format = "fallback",
--- env/.config/nvim/lua/anadai/lazy/lsp.lua
+++ env/.config/nvim/lua/anadai/lazy/lsp.lua
@@ -1,44 +1,47 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
+    {
+        "mason-org/mason.nvim",
+        cmd = "Mason",
+        build = ":MasonUpdate",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        },
     },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim" },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer", 
-        "ts_ls",
-        "pylsp",
-        "gopls",
-        "pyright"
-      },
-      automatic_installation = false,
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            {"mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "ts_ls",
+                "pylsp",
+                "gopls",
+                "pyright"
+            },
+            automatic_enable = true
+        },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "mason.nvim",
-      "mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "mason.nvim",
+            "mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            require("anadai.lazy.lsp.handlers").setup()
+            require("anadai.lazy.lsp.servers").setup()
+        end,
     },
-    config = function()
-      require("anadai.lazy.lsp.handlers").setup()
-      require("anadai.lazy.lsp.servers").setup()
-    end,
-  },
 }
